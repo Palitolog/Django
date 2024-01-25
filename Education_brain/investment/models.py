71151bdd7c12c5ae424728386875191e3e5c3105
Palitolog <petya.lobutev123123@gmail.com>
2024-01-25 20:20:20 +0900

    add settings investment

--- Education_brain/investment/models.py
+++ Education_brain/investment/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):   # models.Model означает, что объект Post является моделью Django, так Django поймет, что он должен сохранить его в базу данных.
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # ссылка на другую модель.
+    title = models.CharField(max_length=200)    # так мы определяем текстовое поле с ограничением на количество символов.
+    text = models.TextField()   # так определяется поле для неограниченно длинного текста. Выглядит подходящим для содержимого поста, верно?
+    created_date = models.DateTimeField(default=timezone.now)   # дата и время.
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
--- Education_brain/settings.py
+++ Education_brain/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'investment',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
